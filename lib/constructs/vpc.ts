e9c571d9bcd34f319ab29df05bee1a5d22cc6c31
Flojolo <florian1siegel@googlemail.com>
2024-07-15 17:48:30 +0200

    remove non isolated subnets used for exploration

--- lib/constructs/vpc.ts
+++ lib/constructs/vpc.ts
@@ -7,14 +7,6 @@ export class VpcConstruct extends Construct {
     cidr: "10.0.0.0/16",
     subnetConfiguration: [
       {
-        name: "public",
-        subnetType: ec2.SubnetType.PUBLIC,
-      },
-      {
-        name: "private",
-        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-      },
-      {
         name: "private-isolated",
         subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
       },
